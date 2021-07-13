e18703dbfc27c31530bff2a48cec0c4b1902d47f
Cheng Pan <61174986+cpan1995@users.noreply.github.com>
2021-07-13 18:07:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ function getRestOfPages(searchObject, storePageNumber,searchValue){
 
 //We'll call the init function once the DOM is loaded
 document.addEventListener('DOMContentLoaded', () => {
-  const apiKey = '799cdd3c'
+  const apiKey = '[UR KEY]'
   init(apiKey);
 })
 
